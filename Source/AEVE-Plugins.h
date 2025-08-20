e7bf2c896b1d18374c89fd04d7eb23d0c2a43fcd
Hashory <89839285+Hashory@users.noreply.github.com>
2025-08-20 14:40:32 +0900

    feat: add .clang-format and .clang-format-ignore for consistent code styling; configure pre-commit hook for clang-format

--- Source/AEVE-Plugins.h
+++ Source/AEVE-Plugins.h
@@ -10,11 +10,13 @@
 #include <Windows.h>
 #endif
 
+// clang-format off
 #include "entry.h"
 #include "AE_Effect.h"
 #include "A.h"
 #include "AE_EffectCB.h"
 #include "AE_Macros.h"
 #include "Param_Utils.h"
+// clang-format on
 
 // Effects includes below
